48248c06ea9269471d9d4318e1fdb2c8d3f20150
fil-fyodorov <fil-fyodorov@yandex.ru>
2024-12-15 16:35:47 +0300

    add about

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
+about_me = {
+   "name": "Фдоров",
+   "surname": "Филипп",
+   "email": "fil-fyodorov@yandex.ru"
+}
+
+
+@app.route("/about")
+def about():
+   return about_me
 
 
 @app.route("/")  # Первый URL для обработки
