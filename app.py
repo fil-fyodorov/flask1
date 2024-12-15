d5518ef7855029758fb6cd388360e61c9f828bd1
fil-fyodorov <fil-fyodorov@yandex.ru>
2024-12-15 17:07:08 +0300

    add quotes

--- app.py
+++ app.py
@@ -4,15 +4,45 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
 about_me = {
-   "name": "Фдоров",
+   "name": "Федоров",
    "surname": "Филипп",
    "email": "fil-fyodorov@yandex.ru"
 }
 
 
+quotes_text = [
+   {
+       "id": 3,
+       "author": "Rick Cook",
+       "text": "Программирование сегодня — это гонка разработчиков программ, стремящихся писать программы с большей и лучшей идиотоустойчивостью, и вселенной, которая пытается создать больше отборных идиотов. Пока вселенная побеждает."
+   },
+   {
+       "id": 5,
+       "author": "Waldi Ravens",
+       "text": "Программирование на С похоже на быстрые танцы на только что отполированном полу людей с острыми бритвами в руках."
+   },
+   {
+       "id": 6,
+       "author": "Mosher’s Law of Software Engineering",
+       "text": "Не волнуйтесь, если что-то не работает. Если бы всё работало, вас бы уволили."
+   },
+   {
+       "id": 8,
+       "author": "Yoggi Berra",
+       "text": "В теории, теория и практика неразделимы. На практике это не так."
+   },
+
+]
+
+
 @app.route("/about")
 def about():
    return about_me
+
+
+@app.route("/quotes")
+def quotes():
+   return quotes_text
 
 
 @app.route("/")  # Первый URL для обработки
